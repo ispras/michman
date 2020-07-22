c2712a253a8f4888dffd7b0c67c82cd9fb648274
Biryukov Andrew <biryukov.andrej2000@ispras.ru>
2020-07-22 15:38:01 +0300

    add TestImageDelete

--- rest/handlers/image_test.go
+++ rest/handlers/image_test.go
@@ -5,7 +5,7 @@ import (
 	//"encoding/json"
 	"log"
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest" 
 	"os"
 	"testing"
 
@@ -55,4 +55,50 @@ func TestImageGet(t *testing.T) {
 	if response.Code != http.StatusOK {
 		t.Fatalf("Expected status code %v, but received: %v", "200", response.Code)
 	}
+}
+
+func TestImageDelete(t *testing.T) {
+	l := log.New(os.Stdout, "TestImageDelete: ", log.Ldate|log.Ltime)
+	mockCtrl := gomock.NewController(t)
+	mockClient := mocks.NewMockGrpcClient(mockCtrl)
+	mockDatabase := mocks.NewMockDatabase(mockCtrl)
+	imageName := "testImageName"
+	errHandler := HttpErrorHandler{}
+
+	t.Run ("Image isn't used", func(t *testing.T){
+		request, _ := http.NewRequest("PUT", "/images/"+imageName, nil)
+		response := httptest.NewRecorder()
+
+		mockDatabase.EXPECT().ListClusters().Return([]protobuf.Cluster{}, nil)
+		mockDatabase.EXPECT().ListProjects().Return([]protobuf.Project{}, nil)
+		mockDatabase.EXPECT().DeleteImage(gomock.Any()).Return(nil)
+
+		hS := HttpServer{Gc: mockClient, Logger: l, Db: mockDatabase, ErrHandler: errHandler}
+		hS.ImageDelete(response, request, httprouter.Params{{Key: "imageName", Value: imageName}})
+
+		if response.Code != http.StatusOK {
+			t.Fatalf("Expected status code %v, but received: %v", http.StatusOK, response.Code)
+		}
+	})
+
+	t.Run ("Image is used", func(t *testing.T){
+		request, _ := http.NewRequest("PUT", "/images/"+imageName, nil)
+		response := httptest.NewRecorder()
+
+		var existedCluster = []protobuf.Cluster{protobuf.Cluster{Name: "Name2",
+			ID: "some_ID_123", ProjectID: "test-TEST-UUID-123", Image:imageName }}
+
+		var existedProject = []protobuf.Project{protobuf.Project{Name: "Name1",
+			ID: "some_ID_124", DefaultImage: imageName}}
+
+		mockDatabase.EXPECT().ListClusters().Return(existedCluster, nil)
+		mockDatabase.EXPECT().ListProjects().Return(existedProject, nil)
+
+		hS := HttpServer{Gc: mockClient, Logger: l, Db: mockDatabase, ErrHandler: errHandler}
+		hS.ImageDelete(response, request, httprouter.Params{{Key: "imageName", Value: imageName}})
+
+		if response.Code != http.StatusBadRequest {
+			t.Fatalf("Expected status code %v, but received: %v", http.StatusBadRequest, response.Code)
+		}
+	})
 }
